6173d82aea6510c283290e2eb7357647e44515f2
Quentin Henriet <quentin.henriet@free.fr>
2017-05-19 17:31:54 +0200

    Add an initial gui::Window class. The game now depends on tinyXML2.

---
+++ src/gui/window.cpp
@@ -0,0 +1,249 @@
+
+/*
+    Devilsai - A game written using the SFML library
+    Copyright (C) 2009-2017  Quentin Henriet
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <tinyxml2.h>
+
+#include "tools/signals.h"
+
+#include "imageManager/imageManager.h"
+
+#include "gui/window.h"
+#include "gui/textWidget.h"
+#include "gui/button.h"
+
+#include "config.h"
+
+using namespace tinyxml2;
+
+namespace gui{
+
+void Window::setTopLeftCoordinates(int x, int y)
+{
+    xTopLeft = x;
+    yTopLeft = y;
+    xCenter = 0;
+    yCenter = 0;
+}
+
+void Window::setCenterCoordinates(int x, int y)
+{
+    xCenter = x;
+    yCenter = y;
+    xTopLeft = 0;
+    yTopLeft = 0;
+}
+
+void Window::setSize(int w, int h)
+{
+    width = w;
+    height = h;
+}
+
+int Window::getXTopLeft()
+{
+    if (xTopLeft == 0)
+        return xCenter - width/2;
+
+    return xTopLeft;
+}
+
+int Window::getYTopLeft()
+{
+    if (yTopLeft == 0)
+        return yCenter - height/2;
+
+    return yTopLeft;
+}
+
+int Window::getXCenter()
+{
+    if (xCenter == 0)
+        return xTopLeft + width/2;
+
+    return xCenter;
+}
+
+int Window::getYCenter()
+{
+    if (yCenter == 0)
+        return yTopLeft + height/2;
+
+    return yCenter;
+}
+
+void Window::startWindow()
+{
+    exitWindow = false;
+}
+
+void Window::display(RenderWindow& app)
+{
+    imageManager::display(app, "misc", background, getXTopLeft(), getYTopLeft());
+
+    for (auto& widget : widgets)
+    {
+        widget.second->display(app);
+    }
+}
+
+void Window::manage(RenderWindow& app)
+{
+    while (!exitWindow)
+    {
+        Event event;
+        while (app.pollEvent(event))
+        {
+            manage(app, event);
+        }
+
+        display(app);
+        app.display();
+    }
+}
+
+void Window::manage(RenderWindow& app, Event &event)
+{
+    for (auto& widget : widgets)
+    {
+        if (widget.second->activated(app, event.type))
+        {
+            for (auto& signal : signals)
+            {
+                if (signal.first == widget.first)
+                {
+                    tools::signals::addSignal(signal.second);
+                }
+            }
+            for (auto& exitWindowSignal : exitWindowSignals)
+            {
+                if (exitWindowSignal == widget.first)
+                {
+                    exitWindow = true;
+                }
+            }
+        }
+    }
+}
+
+void Window::loadFromFile(string path)
+{
+    path = INSTALL_DIR + path;
+
+    XMLDocument file;
+    file.LoadFile(path.c_str());
+
+    XMLHandle hdl(file);
+    XMLElement *elem = hdl.FirstChildElement().FirstChildElement().ToElement();
+
+    while (elem)
+    {
+        string elemName = elem->Name();
+
+        if (elemName == "properties")
+        {
+            if (elem->Attribute("xTopLeft"))
+            {
+                int x = 0, y = 0;
+                elem->QueryAttribute("xTopLeft", &x);
+                elem->QueryAttribute("yTopLeft", &y);
+                setTopLeftCoordinates(x, y);
+            }
+            if (elem->Attribute("xCenter"))
+            {
+                int x = 0, y = 0;
+                elem->QueryAttribute("xCenter", &x);
+                elem->QueryAttribute("yCenter", &y);
+                setCenterCoordinates(x, y);
+            }
+
+            int w = 0, h = 0;
+            elem->QueryAttribute("width", &w);
+            elem->QueryAttribute("height", &h);
+            setSize(w, h);
+
+            background = elem->Attribute("background");
+        }
+
+        if (elemName == "widget")
+        {
+            string name = elem->Attribute("name");
+            string type = elem->Attribute("type");
+            Widget* widget = nullptr;
+            if (type == "text")
+            {
+                widget = new TextWidget();
+                widgets.insert(map<string, Widget*>::value_type(name, widget));
+            }
+            if (type == "button")
+            {
+                widget = new Button();
+                widgets.insert(map<string, Widget*>::value_type(name, widget));
+            }
+
+            if (elem->Attribute("xTopLeft"))
+            {
+                int x = 0, y = 0;
+                elem->QueryAttribute("xTopLeft", &x);
+                elem->QueryAttribute("yTopLeft", &y);
+                widget->setTopLeftCoordinates(getXTopLeft() + x, getYTopLeft() + y);
+            }
+            if (elem->Attribute("xCenter"))
+            {
+                int x = 0, y = 0;
+                elem->QueryAttribute("xCenter", &x);
+                elem->QueryAttribute("yCenter", &y);
+                widget->setCenterCoordinates(getXTopLeft() + x, getYTopLeft() + y);
+            }
+
+            int w = 0, h = 0;
+            elem->QueryAttribute("width", &w);
+            elem->QueryAttribute("height", &h);
+            widget->setSize(w, h);
+
+            if (elem->Attribute("allBackground"))
+            {
+                string b = elem->Attribute("allBackground");
+                widget->setAllBackground(b);
+            }
+
+            if (elem->Attribute("allText"))
+            {
+                string t = elem->Attribute("allText");
+//                 widget->setAllText(t);
+            }
+        }
+
+        if (elemName == "signal")
+        {
+            string emitter = elem->Attribute("emitter");
+            string signal = elem->Attribute("signal");
+            signals.push_back(pair<string, string>(emitter, signal));
+        }
+
+        if (elemName == "exitWindowSignal")
+        {
+            string emitter = elem->Attribute("emitter");
+            exitWindowSignals.push_back(emitter);
+        }
+
+        elem = elem->NextSiblingElement();
+    }
+}
+
+} //namespace gui
---
+++ src/gui/window.h
@@ -0,0 +1,73 @@
+
+/*
+    Devilsai - A game written using the SFML library
+    Copyright (C) 2009-2016  Quentin Henriet
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef GUI_WINDOW_H
+#define GUI_WINDOW_H
+
+#include <string>
+#include <list>
+#include <map>
+
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "gui/widget.h"
+
+using namespace std;
+using namespace sf;
+
+namespace gui{
+
+class Window
+{
+    private:
+        map < string, Widget* > widgets;
+        list < pair<string, string> > signals;
+        list < string > exitWindowSignals;
+
+        bool exitWindow = false;
+
+        int xTopLeft = 0, yTopLeft = 0;
+        int xCenter = 0, yCenter = 0;
+        int width = 0, height = 0;
+
+        string background;
+
+        void setTopLeftCoordinates(int x, int y);
+        void setCenterCoordinates(int x, int y);
+        void setSize(int w, int h);
+
+        int getXTopLeft();
+        int getYTopLeft();
+        int getXCenter();
+        int getYCenter();
+
+    public:
+        void startWindow();
+        void display(RenderWindow& app);
+        void manage(RenderWindow& app);
+        void manage(RenderWindow& app, Event &event);
+
+        void loadFromFile(string path);
+};
+
+} //namespace gui
+
+#endif // GUI_WINDOW_H
+
