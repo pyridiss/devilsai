pyridiss/devilsai

commit 6a9456e67ff57de6875bd5dc81e74c676fd95ea9

Individu_Unique::loadFromXML() manage the finding of the data by itself instead of calling gamedata::loadFromXML() which had to find the unique and call Individu_Unique::loadFromXML() again.

src/ElementsCarte/IndividusUniques.cpp
@@ -21,6 +21,7 @@
 
 #include <tinyxml2.h>
 
+#include "tools/debug.h"
 #include "tools/filesystem.h"
 #include "tools/timeManager.h"
 #include "tools/textManager.h"
@@ -132,16 +133,13 @@ void Individu_Unique::loadFromXML(XMLHandle &handle)
 {
     XMLElement *elem = handle.ToElement();
 
+    //May be needed if loadFromDataFile is set.
+    XMLDocument file;
+
     if (elem->Attribute("name"))
     {
         Type = elem->Attribute("name");
         Nom = tools::textManager::getText("species", Type);
-    }
-
-    if (elem->Attribute("loadFromDataFile"))
-    {
-        dataFile = elem->Attribute("loadFromDataFile");
-        gamedata::loadFromXML(tools::filesystem::dataDirectory(), dataFile);
     }
 
     double x = 0, y = 0;
@@ -152,7 +150,32 @@ void Individu_Unique::loadFromXML(XMLHandle &handle)
     if (elem->Attribute("tag"))
         Liste = elem->Attribute("tag");
 
-    elem = handle.FirstChildElement().ToElement();
+    if (elem->Attribute("loadFromDataFile"))
+    {
+        //Another file must be loaded to complete the unique profile.
+        dataFile = elem->Attribute("loadFromDataFile");
+
+        //First, give this file to gamedata because it can contain species used by the unique
+        gamedata::loadFromXML(tools::filesystem::dataDirectory(), dataFile);
+
+        //We need to load this file and find the XMLElement named "uniqueData" to continue the loading
+        file.LoadFile((tools::filesystem::dataDirectory() + dataFile).c_str());
+
+        XMLHandle hdl(file);
+        elem = hdl.FirstChildElement().FirstChildElement("uniqueData").FirstChildElement().ToElement();
+
+        if (elem == nullptr)
+        {
+            tools::debug::error("Cannot find unique data", "files", __FILENAME__, __LINE__);
+            return;
+        }
+    }
+    else
+    {
+        //No other file needed, we can continue with this file
+        elem = handle.FirstChildElement().ToElement();
+    }
+
     while (elem)
     {
         string elemName = elem->Name();

src/gamedata.cpp
@@ -318,18 +318,6 @@ void loadFromXML(const string& dataDirectory, const string& mainFile)
             }
         }
 
-        if (elemName == "uniqueData")
-        {
-            string uniqueName = elem->Attribute("name");
-
-            Individu_Unique* u = findIndividuUnique(uniqueName);
-            if (u != nullptr)
-            {
-                XMLHandle hdl2(elem);
-                u->loadFromXML(hdl2);
-            }
-        }
-
         if (elemName == "loadWorld")
         {
             string worldFile = elem->Attribute("file");

commit 611f719df0a2723c6d1b3b109411ce0768c0a342

Fix bug introduced by 6a9456e6: Individu_Unique::loadFromXML() was unable to load saved data.

src/ElementsCarte/IndividusUniques.cpp
@@ -132,9 +132,6 @@ void Individu_Unique::loadFromXML(XMLHandle &handle)
 {
     XMLElement *elem = handle.ToElement();
 
-    //May be needed if loadFromDataFile is set.
-    XMLDocument file;
-
     if (elem->Attribute("name"))
     {
         Type = elem->Attribute("name");
@@ -158,22 +155,16 @@ void Individu_Unique::loadFromXML(XMLHandle &handle)
         gamedata::loadFromXML(tools::filesystem::dataDirectory(), dataFile);
 
         //We need to load this file and find the XMLElement named "uniqueData" to continue the loading
+        XMLDocument file;
         file.LoadFile((tools::filesystem::dataDirectory() + dataFile).c_str());
-
         XMLHandle hdl(file);
-        elem = hdl.FirstChildElement().FirstChildElement("uniqueData").FirstChildElement().ToElement();
+        XMLHandle hdl2 = hdl.FirstChildElement().FirstChildElement("uniqueData");
 
-        if (elem == nullptr)
-        {
-            tools::debug::error("Cannot find unique data", "files", __FILENAME__, __LINE__);
-            return;
-        }
+        //We can call loadFromXML() with this other file
+        loadFromXML(hdl2);
     }
-    else
-    {
-        //No other file needed, we can continue with this file
-        elem = handle.FirstChildElement().ToElement();
-    }
+
+    elem = handle.FirstChildElement().ToElement();
 
     while (elem)
     {